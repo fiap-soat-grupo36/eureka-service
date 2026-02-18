3b6519ada9d37c10a17e5f80a049b9119f53873c
nessaoana <nessaoana09@gmail.com>
2026-02-18 13:17:35 -0300

    Update Terraform configuration for production deployment and adjust secrets management

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,15 @@
 terraform {
   backend "s3" {
-    bucket = "fiap-fase-4-oficina"
-    key    = "eureka-service/terraform.tfstate"
-    region = "us-east-1"
+    bucket                      = "terraform-state-fiap"
+    key                         = "eureka-service/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoints                   = { s3 = "http://localhost:4566" }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    access_key                  = "test"
+    secret_key                  = "test"
   }
 }
